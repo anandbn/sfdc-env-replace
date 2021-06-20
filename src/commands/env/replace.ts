d1c6d3dcd6b81c0e73949bac5e5dd31f4388f81f
Anand Bashyam-Narasimhan <anarasimhan@anarasimhan-ltm.internal.salesforce.com>
2021-06-20 09:54:46 -0400

    Moved config to YAML format

--- src/commands/env/replace.ts
+++ src/commands/env/replace.ts
@@ -1,6 +1,8 @@
+import { string } from '@oclif/command/lib/flags';
 import { flags, SfdxCommand } from '@salesforce/command';
 import { Messages, SfdxError } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
+const YAML = require('yaml')
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
@@ -23,7 +25,7 @@ export default class Replace extends SfdxCommand {
 
   protected static flagsConfig = {
     basedir: flags.string({ char: 'd', description: messages.getMessage('basedirFlagDescription') }),
-    testmode: flags.boolean({ char: 't', description: messages.getMessage('testmodeFlagDescription')}),
+    testmode: flags.boolean({ char: 't', description: messages.getMessage('testmodeFlagDescription') }),
     // flag with a value (-n, --name=VALUE)
     replaceconfig: flags.string({ char: 'c', description: messages.getMessage('replaceconfigFlagDescription') })
   };
@@ -38,32 +40,74 @@ export default class Replace extends SfdxCommand {
   protected static requiresProject = false;
 
   public async run(): Promise<AnyJson> {
-    let replaceConfigs: any[] = JSON.parse(fs.readFileSync(this.flags.replaceconfig, { encoding: 'utf8', flag: 'r' }));
 
-    for (let i = 0; i < replaceConfigs.length; i++) {
-      let sourceFile: string = `${this.flags.basedir}${path.sep}${replaceConfigs[i].sourceFile}`;
-      let destFile: string = `${sourceFile}${this.flags.testmode?'.new':''}`;
+    // The type we are querying for
+    interface ReplaceConfig {
+      regex_lib: Record<string, string>[],
+      rules: Record<string, ReplaceConfigRules>
+    }
 
-      this.ux.log(`Replacing environment variables for ${sourceFile}`);
-      let fileData: string = fs.readFileSync(sourceFile, { encoding: 'utf8', flag: 'r' });
+    // The type we are querying for
+    interface ReplaceConfigRules {
+      extensions: string[],
+      files: string[],
+      regex_name: string,
+      replace_values: string[]
+      replace_with: string
+    }
+    //let replaceConfigs: any[] = JSON.parse(fs.readFileSync(this.flags.replaceconfig, { encoding: 'utf8', flag: 'r' }));
+    let replaceConfigs: ReplaceConfig = YAML.parse(fs.readFileSync(this.flags.replaceconfig, { encoding: 'utf8', flag: 'r' }));
+
+    //for (let i = 0; i < replaceConfigs.rules.keys.length(); i++) {
+    for (let key in replaceConfigs.rules) {
+      let ruleRegex: string = replaceConfigs.rules[key].regex_name;
+      let regExpr = replaceConfigs.regex_lib[ruleRegex];
+      this.replaceValuesForRule(key, replaceConfigs.rules[key], regExpr);
+
+    }
+
+    // Return an object to be displayed with --json
+    return { 'status': 'ok' };
+  }
+
+  private async replaceValuesForRule(ruleName: string, replaceConfig: Record<string, any>, regExpr: string) {
+
+    for (let sourceFile of replaceConfig.files) {
+      let sourceFileAbsPath: string = `${this.flags.basedir}${path.sep}${sourceFile}`;
+      let destFile: string = `${sourceFileAbsPath}${this.flags.testmode ? '.new' : ''}`;
+      let fileData: string = fs.readFileSync(sourceFileAbsPath, { encoding: 'utf8', flag: 'r' });
       fileData = fileData.replace(/(\n(\s+)<)/gm, "<");
       fileData = fileData.replace(/(\n<)/gm, "<");
-      this.ux.log(`${sourceFile}: Unformatted XML file`);
-      for (let j = 0; j < replaceConfigs[i].replacements.length; j++) {
-        this.ux.log(`${sourceFile}: Replacing ${replaceConfigs[i].replacements[j].findText} with ${replaceConfigs[i].replacements[j].replaceText} `);
-        fileData = fileData.replace(replaceConfigs[i].replacements[j].findText,replaceConfigs[i].replacements[j].replaceText);
-      }
+      this.ux.log(`Rule[${ruleName}] - ${sourceFile} - Unformatted XML at ${sourceFileAbsPath}`);
+      if (replaceConfig.replace_values) {
+        for (let replaceVal of replaceConfig.replace_values) {
+          let finalRegex: string = regExpr.replace('__REPLACE_VALUE__', replaceVal);
+          this.ux.log(`Rule[${ruleName}] - ${sourceFile} - Using Regular Expression : ${finalRegex}`);
+          this.ux.log(`Rule[${ruleName}] - ${sourceFile} - Absolute file path ${sourceFileAbsPath}`);
+          this.ux.log(`Rule[${ruleName}] - ${sourceFile} - Replacing ${replaceVal} with ''`);
+          fileData = fileData.replace(finalRegex, '');
+        }
+      } else {
+        this.ux.log(`Rule[${ruleName}] - ${sourceFile} - No replacement values found !!! `);
+        let finalRegex: string = regExpr;
+        let replaceWith:string = replaceConfig.replace_with || '';
+        this.ux.log(`Rule[${ruleName}] - ${sourceFile} - Using Regular Expression : ${finalRegex}`);
+        this.ux.log(`Rule[${ruleName}] - ${sourceFile} - Absolute file path ${sourceFileAbsPath}`);
+        this.ux.log(`Rule[${ruleName}] - ${sourceFile} - Replacing with '${replaceWith}'`);
+        fileData = fileData.replace(finalRegex, replaceWith);
 
+      }
       fileData = formatXml(fileData, {
         collapseContent: true,
         lineSeparator: '\n'
       });
       this.ux.log(`${sourceFile}: Formatted to XML `);
       fs.writeFileSync(`${destFile}`, fileData, { encoding: 'utf8' });
-      this.ux.log(`${sourceFile}: Written  file to ${destFile}`);
+      this.ux.log(`Rule[${ruleName}] - ${sourceFile} - Written file to ${destFile}`);
     }
 
-    // Return an object to be displayed with --json
-    return { 'status': 'ok' };
+
+
   }
+
 }
